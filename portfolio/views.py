847a8f00a0ec49b330307f525dbb8e31080501db
snghhrsmrn <snghhrsmrn@gmail.com>
2021-02-20 16:29:57 +0530

    project sorted by id

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Project
 
 def home(request):
-    projects = Project.objects.all()
+    projects = Project.objects.order_by('-id').all()
     return render(request, 'portfolio/home.html', {'projects':projects})
